55bf228efd5c8d0c74bf66d07e1e8f70065afd18
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-09-19 01:20:56 +0700

    feat: add attribute for Entity Customer

--- src/main/java/com/example/springmall/entity/Customer.java
+++ src/main/java/com/example/springmall/entity/Customer.java
@@ -1,4 +1,25 @@
 package com.example.springmall.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customer")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 100, nullable = false)
+    private String email;
+
+
 }
