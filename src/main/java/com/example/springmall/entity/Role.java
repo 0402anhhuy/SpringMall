b96e9f4de5d29ed8b3d7cae8f34068cedf45bace
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-09-19 01:15:12 +0700

    feat: add Entity Role

--- src/main/java/com/example/springmall/entity/Role.java
+++ src/main/java/com/example/springmall/entity/Role.java
@@ -1,5 +1,20 @@
 package com.example.springmall.entity;
 
-public class Role {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "role")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer roleId;
+
+    @Column(nullable = false,  unique = true)
+    private String roleName;
 }
