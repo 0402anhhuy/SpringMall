0d8918c539af254f37968135233cc1b641d6bf9d
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-09-18 12:26:37 +0700

    feat: add Entity Wishlist

--- src/main/java/com/example/springmall/entity/Wishlist.java
+++ src/main/java/com/example/springmall/entity/Wishlist.java
@@ -1,4 +1,19 @@
 package com.example.springmall.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "wishlist")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Wishlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
 }
