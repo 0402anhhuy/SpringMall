6750ddf8a293f7463a073625a2f82acbbeb072f2
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-09-21 15:33:14 +0700

    feat: add Entity Customer

--- src/main/java/com/example/springmall/entity/Customer.java
+++ src/main/java/com/example/springmall/entity/Customer.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Table(name = "customer")
 @Data
@@ -15,9 +17,27 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+
     @Column(length = 100, nullable = false)
-    private String name;
+    private String fullname;
+
+    @Column(length = 10, nullable = false, unique = true)
+    private String phone;
 
     @Column(length = 100, nullable = false)
     private String email;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String dateOfBirth;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ViewedProduct> viewedProducts;
 }
