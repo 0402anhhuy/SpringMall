b3f242ab52b0697ae6ea4a013ad50d42887181e7
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-09-22 19:18:55 +0700

    feat: update LoginController

--- src/main/java/com/example/springmall/controller/LoginController.java
+++ src/main/java/com/example/springmall/controller/LoginController.java
@@ -1,9 +1,15 @@
 package com.example.springmall.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("/acount")
 public class LoginController {
+
+    @GetMapping("/account/login")
+    public String login() {
+        return "account/login";
+    }
 }
